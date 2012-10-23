b18346c2f2349a6a45c10422b8b431af42838f0f
Jon Latorre <moebius@etxea.net>
2012-10-23 17:59:19 +0200

    Es obligatorio estar logeado para poder ver la lista de examenes y cerrando el registro de uevos usuarios

--- apps/examenes/urls.py
+++ apps/examenes/urls.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls.defaults import *
 from django.views.generic.simple import direct_to_template
+from django.contrib.auth.decorators import login_required, permission_required
 from views import *
 
 urlpatterns = patterns("",
     url(r"^$", direct_to_template, {"template": "examenes/about.html"}, name="indice_examenes"),
     url(r"^subir/$", ExamenCreate.as_view(), name="upload_examen"),
-    url(r"^lista/$", ExamenList.as_view(), name="lista_examenes"),
+    url(r"^lista/$", login_required(ExamenList.as_view()), name="lista_examenes"),
 )
